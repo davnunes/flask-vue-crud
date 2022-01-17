3575ba2330ebb2bcd1afe190175b0e39f6b46c05
davi <davinunes.in@gmail.com>
2022-01-17 17:45:27 -0300

    feat: first commit

---
+++ client/src/components/Ping.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: 'Hello',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/ping';
+
+      axios.get(path).then((res) => {
+        this.msg = res.data;
+      }).catch((error) => console.error(error));
+
+      // fetch(path).then(response => response.json())
+      //   .then(json => {
+      //     this.msg = json.data;
+      //   });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
